8163fd048365c415ed801a480ac6a2b3eaa298ab
onishi-teppei <teppei99.nxw@gmail.com>
2024-08-03 10:52:00 +0900

    バリデーション追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,6 @@
 class Order < ApplicationRecord
+  validation :name, presence: true, length: { maximum: 40 }
+  validation :email, presence: true, length: { maximum: 100 }
+  validation :telephone, presence: true, length: { maximum: 11 }, numericality: { only_integer: true }
+  validation :delivery_address, presence: true, length: { maximum: 100 }
 end
