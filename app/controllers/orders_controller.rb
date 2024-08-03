04e78bc82ca6d65649179d41571104e77f06debf
onishi-teppei <teppei99.nxw@gmail.com>
2024-08-03 10:27:27 +0900

    セッション追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,9 +10,19 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(order_params) # order_paramsから受け取った値を使ってOrderクラスの新しいインスタンスを作成。戻るボタンを押した場合でも、入力した値を保持する
     return render :new if params[:button] == 'back' # ボタンが「戻る」の場合、newアクションを再度実行
-    return redirect_to complete_orders_url if @order.save # 保存に成功した場合、completeアクションにリダイレクト
+    if @order.save # 保存に成功した場合、completeアクションにリダイレクト
+      session[:order_id] = @order.id
+      return redirect_to complete_orders_url
+    end
 
     render :confirm # 保存に失敗した場合、newアクションを再度実行
+  end
+
+  def complete
+    @order = Order.find_by(id: session[:order_id]) # session[:order_id]からOrderクラスのインスタンスを取得
+    return redirect_to new_order_url if @order.blank? # インスタンスがnilの場合、newアクションにリダイレクト
+
+    session[:order_id] = nil # session[:order_id]をnilにする
   end
 
   private # クラス内でのみ使用可能、この下に書かれたメソッドは、外部から呼び出せないようにする
