477e1cd5a8add092b67f35f29402cc6e72a1a323
大西哲平 <oonishiteppei@oonishipeinoMBP.lan>
2024-10-04 23:16:03 +0900

    ラジオボタン追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,6 +9,7 @@ class Order < ApplicationRecord
   validates :delivery_address, presence: true, length: { maximum: 100 }
   validates :payment_method_id, presence: true
   validates :other_comment, length: { maximum: 1_000 }
+  validates :direct_mail_enabled, inclusion: { in: [true, false], message: 'について選択してください' }
 
   after_initialize :format_telephone
   after_initialize :format_email
