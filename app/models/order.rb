6c9c4f890eeeaeaf7ed10d1c88d12e720613a72e
onishi-teppei <teppei99.nxw@gmail.com>
2025-01-02 20:36:58 +0900

    migrationファイル、model作成

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,7 @@ class Order < ApplicationRecord
   belongs_to :payment_method
   has_many :order_inflow_sources
   has_many :inflow_sources, through: :order_inflow_sources
+  has_many :order_products
 
   validates :name, presence: true, length: { maximum: 40 }
   validates :email, presence: true, length: { maximum: 100 }, email_format: true
---
+++ app/models/order_product.rb
@@ -0,0 +1,10 @@
+class OrderProduct < ApplicationRecord
+  belongs_to :order
+  belongs_to :product
+
+  validates :product_id, presence: true
+  validates :quantity, presence: true, numericality: { in: 1..20 }
+
+  delegate :name, :name_and_price_text, to: :product
+
+end
